d3c83a9f70159e6442ae3b5cb3fc890a39ee6f5f
Genicleito Gonçalves <genicleito@live.com>
2020-07-12 20:20:32 -0300

    updated hide_input_cells_notebook_html.py

--- hide_input_cells_notebook_html.py
+++ hide_input_cells_notebook_html.py
@@ -9,7 +9,7 @@ def hide_input_cells_notebook_html(read_path, write_path, title=None):
     c_close = 0
     f = open(write_path, encoding='utf8', mode='w')
     for x in arq:
-        if x == "\n" or ((not "<div class=\"input\">" in x and not " output_stderr" in x) and insere):
+        if (not "<div class=\"input\">" in x and not " output_stderr" in x) and insere:
             if title and "<title>" in x:
                 x = "<title>" + title + "</title>"
             f.write(re.sub("Out\[\d+\]\:", "", x))
@@ -17,11 +17,9 @@ def hide_input_cells_notebook_html(read_path, write_path, title=None):
             insere = False
             c_open = 0
             c_close = 0
-            if " output_stderr" in x:
-                c_open = 1
-        elif '<div' in x and insere == False:
+        if '<div' in x and insere == False:
             c_open += 1
-        elif '</div' in x and insere == False:
+        if '</div' in x and insere == False:
             c_close += 1
         if c_open != 0 and c_open == c_close and insere == False:
             insere = True
