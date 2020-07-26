6dba447d981af2f2da5b76d32a0192a6d921a497
Genicleito Gonçalves <genicleito@live.com>
2020-07-25 21:21:34 -0300

    fix: erro no caminho ao obter a data

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ python3 01_extract_boletins.py
 # Obtendo último arquivo salvo
 raw_path=./boletins_sesab/raw/
 last_boletim_path=./boletins_sesab/last_boletim/
-last_file=$raw_path$(for f in `ls -lt $raw_path`; do echo $f; done | grep ".pdf" | head -n 1)
+last_file=$(for f in `ls -lt $raw_path`; do echo $f; done | grep ".pdf" | head -n 1)
 dt_last_file=$(date -d $(date -r $last_boletim_path$last_file +%Y-%m-%d) +%s)
 dt_final_file=$(date -d $(date -r cases-covid19-bahia.csv +%Y-%m-%d) +%s)
 dt_now=$(date -d $(date +%Y-%m-%d) +%s)
@@ -14,7 +14,7 @@ dt_now=$(date -d $(date +%Y-%m-%d) +%s)
 if [ $dt_last_file -eq $dt_now -a $dt_final_file -eq $dt_now ]
 then
 	echo "Dados já estão atualizados!"
-	echo "Data de modificação do ultimo boletim: $(date -r $last_boletim$last_file)"
+	echo "Data de modificação do ultimo boletim: $(date -r $last_boletim_path$last_file)"
 	echo "Data de atualização: $(date -r cases-covid19-bahia.csv)"
 else
 	echo "Novo arquivo encontrado: $last_file";
